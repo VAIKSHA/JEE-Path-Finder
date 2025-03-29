0e9de5a9f98c271aab88717efd89c11262b119f1
Vishal Sharma <vishalsharma@Vishals-MacBook-Air.local>
2025-03-29 20:58:51 +0530

    model created succefully

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-model = joblib.load("model.pkl")  # Load your trained model
+
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Allow requests only from React app
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
+
+
+model = joblib.load("predictor.pkl")  # Load your trained model
 
 class InputData(BaseModel):
     marks: int
@@ -11,8 +26,8 @@ class InputData(BaseModel):
 
 @app.post("/predict/")
 async def predict(data: InputData):
-    # Convert difficulty to numerical value (Example: Easy=1, Moderate=2, Hard=3)
-    difficulty_mapping = {"Easy": 1, "Moderate": 2, "Hard": 3}
+    # Convert difficulty to numerical value (Example: Easy=1, Moderate=2, Hard=0)
+    difficulty_mapping = {"Easy": 1, "Moderate": 2, "Hard": 0}
     difficulty_level = difficulty_mapping.get(data.difficulty, 2)
 
     # Prepare input for model
